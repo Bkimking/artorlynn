b95759933fa3ece01197abc45db357b4e05a4310
Brian Kimathi <brian01kimathi@gmail.com>
2026-06-16 16:00:31 +0300

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        User::create([
             'name' => 'Brian Kimathi',
             'email' => 'brian01kimathi@gmail.com',
             'password' => bcrypt('123456789'),
